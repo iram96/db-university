b5a1b950b3fafa2c28074d59a9773a0aca331f59
iram96 <mariano.maselli@virgilio.it>
2022-03-10 16:56:33 +0100

    Added ex 10/03

--- ciao.sql
+++ ciao.sql
@@ -24,3 +24,18 @@ SELECT COUNT(*) FROM `teachers` GROUP BY `office_address` WHERE COUNT(*) > 1
 SELECT ROUND(AVG(`vote`)) FROM `exam_student` GROUP BY `exam_id`
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(*), `department_id` FROM `degrees` GROUP BY `department_id`
+
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * FROM `degrees` JOIN `students` ON `degrees`.`id` = `students`.`degree_id` WHERE `degrees`.`id` = 53;
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT * FROM `departments` JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id` WHERE `departments`.`id` = 7;
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * FROM `courses` JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id` JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id` WHERE `course_teacher`.`teacher_id` = 44;
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT * FROM `departments` JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id` JOIN `students` ON `degrees`.`id` = `students`.`degree_id` ORDER BY `students`.`surname` ,`students`.`name` ASC;
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `teachers`.`name`, `teachers`.`surname`, `courses`.`name`, `degrees`.`name` FROM `degrees` JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id` JOIN `course_teacher` ON `courses`.`degree_id` = `course_teacher`.`course_id` JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`;
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
+SELECT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` FROM `departments` JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id` JOIN `courses` ON `degrees`.`department_id` = `courses`.`degree_id` JOIN `course_teacher` ON `courses`.`degree_id` = `course_teacher`.`course_id` JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id` WHERE `departments`.`id` = 5;
